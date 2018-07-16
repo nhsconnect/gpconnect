2891db0aeb95d3b0891e0ea96a263665b9bdaec1
Tim Ford <33860197+tim1ford@users.noreply.github.com>
2018-07-16 15:45:30 +0100

    Update customscripts.js

--- js/customscripts.js
+++ js/customscripts.js
@@ -53,24 +53,3 @@ $(function() {
         }
     });
 });
-
-//added to expand rows in table
-$(document).ready(function() {
-  $('.panel-title').click(
-    function() {
-      var contentDiv = $(this).parent().next('.panel-body');
-      var icon = $(this).children('span.icon');
-      var isVisible = contentDiv.is(":visible");
-      if (isVisible)
-      {
-        contentDiv.slideUp();        
-        icon.html('+');
-      }
-      else
-      {
-        contentDiv.slideDown();
-        icon.html('-');        
-      }
-    }
-  );
-});
